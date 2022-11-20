c6fe9eb38cfb9f5380d6f93f1e0689f45f36477b
Chase <chase.harp@gmail.com>
2022-11-20 13:53:39 -0600

    Description will now properly display

--- frontend/src/components/eventDetails.vue
+++ frontend/src/components/eventDetails.vue
@@ -53,9 +53,11 @@
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Description</span>
+              <!-- added event to display the event description -->
               <textarea
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 rows="2"
+                v-model="event.description"
               ></textarea>
             </label>
           </div>
